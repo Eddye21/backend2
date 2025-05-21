309e4f94dfa9b3f41fc670d83f62b7e77e197661
Eduardo Moyla <eduadomoyla@gmail.com>
2025-05-21 19:03:54 -0400

    Solve handlebars

--- src/routes/handlebars/views.router.js
+++ src/routes/handlebars/views.router.js
@@ -3,10 +3,13 @@ import { productManager } from "../../data/managers/db/mongoDb.manager.js";
 
 const viewsRouter = Router()
 
-const getProductsCb = async(req, res, next) => {
-    res.render("index")
-}
-
-viewsRouter.get("/", getProductsCb)
+viewsRouter.get("/", async(req, res) => {
+    try {
+        const products = await productManager.readAll()
+        res.status(200).render("index", products)
+    } catch (error) {
+        console.log(error.message)
+    }
+})
 
 export default viewsRouter
