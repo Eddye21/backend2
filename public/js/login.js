6d90479ca35b6e72292fc63a85ef7e078e417288
Eduardo Moyla <eduadomoyla@gmail.com>
2025-06-14 11:30:23 -0400

    Correct session start check

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,9 @@
+const alertInvalid = () => {
+    Swal.fire({
+        icon: "error",
+        title: "Invalid Credentials"
+    })
+}
 
 let form = document.querySelector(".form-login")
 form.addEventListener("submit", async function (event) {
@@ -6,27 +12,26 @@ form.addEventListener("submit", async function (event) {
     let email = document.querySelector(".input-email").value
     let password = document.querySelector(".input-password").value
 
-    console.log(email)
-    console.log(password)
-
     let data = {
         email: email,
         password: password
     }
 
     try {
-        let response = await fetch("http://localhost:8080/login", {
+        let response = await fetch("http://localhost:8080/api/auth/login", {
             method: "POST",
             headers: {
-                "Content-Type": "aplication/json"
+                "Content-Type": "application/json"
             },
             body: JSON.stringify(data)
-        })
-        let result = await response.json()
-        console.log("Respuesta del servidor", result)
-    } catch (error) {
-        
-    }
+        });
 
-    window.location.href = "http://localhost:8080/";
-})
+        if (response.ok) {
+            window.location.href = "http://localhost:8080/";
+        } else {
+            alertInvalid()
+        }
+    } catch (error) {
+        console.error("Error en la solicitud", error);
+    }
+})  
